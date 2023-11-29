72d7971cae91f5423633ef6f887f215de2a25e6a
Amit-softclique <royamit833@gmail.com>
2023-11-29 20:12:43 +0530

    occurence of each word

---
+++ Practice/occurence_word.js
@@ -0,0 +1,21 @@
+// Occuerence of each word in a sentence.
+
+function occuerenceWord(text) {
+    let arrayString = text.replace(/[^a-zA-Z ]/g,"")
+    .toLowerCase()
+    .split(" ");
+
+    
+    let charMap = new Map();
+    console.log('test', charMap);
+    for (let i=0; i < arrayString.length; i++) {
+        if(charMap.has(arrayString[i])) {
+            charMap.set(arrayString[i], charMap.get(arrayString[i]) + 1);
+        } else {
+            charMap.set(arrayString[i], 1);
+        }
+    }
+    return charMap;
+}
+
+console.log(occuerenceWord("This is see for test to check to see, the chek;"));
