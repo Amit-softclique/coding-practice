389d609f225a3052503095d98ce30ac9aeb29c8d
Amit Roy <royamit833@gmail.com>
2025-06-12 15:50:10 +0530

    Update string_reverse.js

--- Practice/string_reverse.js
+++ Practice/string_reverse.js
@@ -7,3 +7,30 @@ function reverseString(str1) {
 
 const res = reverseString(str);
 console.log(res);
+
+***************** Rev string based on word *********************
+
+function revString() {
+    const fullName = 'Amit Roy Sankrail';
+    let result = "";
+    let word = "";
+    
+    for (let i=0; i<= fullName.length; i++) {
+        const char = fullName[i] || " ";
+        if (char !== " ") {
+            word += char;
+        } else {
+        for(let j= word.length-1; j >=0; j--) {
+            // console.log(word[j]);
+            result += word[j];
+            }
+            
+            result += " ";
+            word = "";
+        }
+    }
+    return result;
+}
+
+console.log(revString())
+//output:: timA yoR liarknaS 
